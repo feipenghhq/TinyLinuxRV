e9225f97785fa6d11773e9f50c03f8285012c56a
Heqing Huang <feipenghhq@gmail.com>
2026-09-13 22:30:00 -0700

    Milestone 6: Add CLINT interrupt tests
    - Exercise machine software and timer interrupts.
    - Verify MSIP and MTIP delivery through the mip CSR.
    - Add test-specific trap initialization and handling.

--- emulator/tests/devices/clint/clint.c
+++ emulator/tests/devices/clint/clint.c
@@ -4,30 +4,91 @@
 #include <stdint.h>
 
 #include "addrmap.h"
+#include "mtrap.h"
 #include "uart16550.h"
+
+#define putstr(str)    uart_putstr(UART0_BASE, str)
+#define putnum(num)    uart_put_num(UART0_BASE, num)
+#define INTERRUPT_MASK (UINT64_C(1) << 63)
+
+#define read_csr(csr, value)                                           \
+    do {                                                               \
+        __asm__ volatile("csrr %0, " #csr : "=r"(value) : : "memory"); \
+    } while (0)
+
+#define extract_mip(mip, bit) (mip & ((UINT64_C(1) << (bit))))
+
+int error = 0;
+int msip  = false;
+int mtip  = false;
+
+int mip_msip = false;
+int mip_mtip = false;
+
+void trap_handler(trap_frame_t *tf) {
+    bool     interrupt = false;
+    uint64_t cause;
+    uint64_t mtimecmp;
+    uint64_t mip_value;
+
+    interrupt = tf->mcause & INTERRUPT_MASK;
+    if (!interrupt) {
+        error++;
+        return;
+    }
+
+    cause = tf->mcause & (INTERRUPT_MASK - 1);
+    switch (cause) {
+    case 3: // MSIP
+        read_csr(mip, mip_value);
+        mip_msip  = extract_mip(mip_value, 3) != 0;
+        msip      = true;
+
+        clint_clear_msip(CLINT_BASE);
+        break;
+
+    case 7: // MTIP
+        read_csr(mip, mip_value);
+        mip_mtip  = extract_mip(mip_value, 7) != 0;
+        mtip = true;
+
+        mtimecmp  = clint_read_mtimecmp(CLINT_BASE);
+        putstr("Current mtimecmp: ");
+        putnum(mtimecmp);
+        putstr("\n");
+        // set mtimecmp to a very high value to disable the interrupt and clear timer
+        clint_set_mtimecmp(CLINT_BASE, UINT64_MAX);
+        clint_set_mtime(CLINT_BASE, 0);
+        break;
+
+    default:
+        error++;
+        break;
+    }
+}
 
 int main(void) {
 
+    // Trigger MSIP
     clint_set_msip(CLINT_BASE);
-    clint_clear_msip(CLINT_BASE);
 
+    // Check current timer
     uint64_t mtime = clint_read_mtime(CLINT_BASE);
-    uart_putstr(UART0_BASE, "Current mtime: ");
-    uart_put_num(UART0_BASE, mtime);
-    uart_putstr(UART0_BASE, "\n");
+    putstr("Current mtime: ");
+    putnum(mtime);
+    putstr("\n");
 
-
+    // Trigger MTIP
     clint_set_mtimecmp(CLINT_BASE, 1000);
-    uint64_t mtimecmp = clint_read_mtimecmp(CLINT_BASE);
-    uart_putstr(UART0_BASE, "Current mtimecmp: ");
-    uart_put_num(UART0_BASE, mtimecmp);
-    uart_putstr(UART0_BASE, "\n");
 
-    clint_set_mtime(CLINT_BASE, 1000);
+    // Check  current timer again after clearing it
     mtime = clint_read_mtime(CLINT_BASE);
-    uart_putstr(UART0_BASE, "Current mtime: ");
-    uart_put_num(UART0_BASE, mtime);
-    uart_putstr(UART0_BASE, "\n");
+    putstr("Current mtime: ");
+    putnum(mtime);
+    putstr("\n");
 
-    return 0;
+    if (msip && mtip && mip_mtip && mip_msip)
+        return error;
+    else
+        return 1;
 }
